ef9c44f86e5f5eb2a284f1219ca3dcf865512326
Didan Adha <didanadha99@gmail.com>
2021-11-09 03:12:02 +0700

    Sync API

--- app/src/main/java/com/dsc/movie/Fragment/PopularMovie.kt
+++ app/src/main/java/com/dsc/movie/Fragment/PopularMovie.kt
@@ -1,22 +1,27 @@
 package com.dsc.movie.Fragment
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
 import com.dsc.movie.Adapter.RecyclerViewAdapter
-import com.dsc.movie.DetailMovie
 import com.dsc.movie.Model.Movie
 import com.dsc.movie.R
+import com.dsc.movie.Util.Url
+import org.json.JSONObject
+
 
 class PopularMovie : Fragment(){
 
-    private val Movie = mutableListOf<Movie>()
+    val Movie = mutableListOf<Movie>()
     private lateinit var adapter: RecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,35 +35,38 @@ class PopularMovie : Fragment(){
     ): View? {
         val view = inflater.inflate(R.layout.fragment_popular_movie, container, false)
 
-        Movie.add(
-            Movie(
-                "popmov",
-                "https://asianwiki.com/images/1/1c/Kingdom-_Ashin_of_the_North-P2.jpg",
-                1
-            )
-        )
-        Movie.add(
-            Movie(
-                "popmov",
-                "https://asianwiki.com/images/1/1c/Kingdom-_Ashin_of_the_North-P2.jpg",
-                2
-            )
-        )
-        Movie.add(
-            Movie(
-                "popmov",
-                "https://asianwiki.com/images/1/1c/Kingdom-_Ashin_of_the_North-P2.jpg",
-                3
-            )
-        )
+        AndroidNetworking.get(Url().getUrl("popular"))
+            .setTag("test")
+            .setPriority(Priority.LOW)
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject?) {
+                    val jmovies = response?.getJSONArray("results")
+                    for(i in 0 until jmovies!!.length()){
+                        val jmovie = jmovies.getJSONObject(i)
+                        Movie.add(
+                            Movie(
+                                jmovie.getString("original_title"),
+                                Url().image_path + jmovie.getString("poster_path"),
+                                jmovie.getInt("id")
+                            )
+                        )
+                        val rv = view.findViewById<RecyclerView>(R.id.rcvPopular)
+                        adapter = RecyclerViewAdapter()
+                        rv.apply {
+                            layoutManager = GridLayoutManager(context, 2)
+                        }
+                        rv.adapter = adapter
+                        adapter.updateData(Movie)
+                    }
+                }
 
-        val rv = view.findViewById<RecyclerView>(R.id.rcvPopular)
-        adapter = RecyclerViewAdapter()
-        rv.apply {
-            layoutManager = GridLayoutManager(context, 2)
-        }
-        rv.adapter = adapter
-        adapter.updateData(Movie)
+                override fun onError(error: ANError) {
+                    Log.e("_error", error.errorBody + error.errorDetail)
+                }
+            })
+
+
 
         return view
     }
